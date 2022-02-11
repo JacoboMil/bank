809573c65e241b96e9da6136337eca30dc2cc7c1
jacobomillan <jacobo.millan@outlook.com>
2022-02-11 17:57:05 +0100

    userResponseConverter added

--- src/main/kotlin/com/iobuilders/bank/user/adapter/in/rest/UserController.kt
+++ src/main/kotlin/com/iobuilders/bank/user/adapter/in/rest/UserController.kt
@@ -4,7 +4,7 @@ import com.iobuilders.bank.api.UsersApi
 import com.iobuilders.bank.model.UserRequest
 import com.iobuilders.bank.model.UserResponse
 import com.iobuilders.bank.user.UserService
-import org.slf4j.LoggerFactory
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
@@ -12,15 +12,15 @@ import java.util.*
 
 @RestController
 class UserController(
-    private val userService: UserService
+    private val userService: UserService,
+    private val userResponseConverter: UserResponseConverter
     ): UsersApi {
 
     companion object {
-        private val log = LoggerFactory.getLogger(UserController::class.java)
+        private val log = getLogger(UserController::class.java)
     }
 
     override fun createUser(userRequest: UserRequest): ResponseEntity<UserResponse> {
-
         log.info("createUser UserRequest:$userRequest")
 
         val newUser = userService.createUser(
@@ -31,29 +31,18 @@ class UserController(
         )
 
         return ResponseEntity(
-            UserResponse(
-                userId = newUser.id,
-                firstName = newUser.firstName,
-                lastName = newUser.lastName,
-                email = newUser.email
-            ),
+            userResponseConverter.convert(newUser),
             HttpStatus.CREATED
         )
     }
 
     override fun getUserById(userId: UUID): ResponseEntity<UserResponse> {
-
         log.info("getUserById userId:$userId")
 
         val user = userService.getUserById(userId)
 
         return ResponseEntity(
-            UserResponse(
-                userId = user.id,
-                firstName = user.firstName,
-                lastName = user.lastName,
-                email = user.email
-            ),
+            userResponseConverter.convert(user),
             HttpStatus.OK
         )
     }
---
+++ src/main/kotlin/com/iobuilders/bank/user/adapter/in/rest/UserResponseConverter.kt
@@ -0,0 +1,19 @@
+package com.iobuilders.bank.user.adapter.`in`.rest
+
+import com.iobuilders.bank.model.UserResponse
+import com.iobuilders.bank.user.model.User
+import org.springframework.core.convert.converter.Converter
+import org.springframework.stereotype.Component
+
+@Component
+class UserResponseConverter : Converter<User, UserResponse> {
+    override fun convert(source: User): UserResponse {
+        return UserResponse(
+            userId = source.id,
+            username = source.username,
+            firstName = source.firstName,
+            lastName = source.lastName,
+            email = source.email
+        )
+    }
+}
